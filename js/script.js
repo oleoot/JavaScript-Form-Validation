0d6a078d0d12e84ddd7ae23f8c57baf6aae1379c
oleoot <oleoot28@gmail.com>
2020-05-23 21:40:52 +0300

    Validating email input

--- js/script.js
+++ js/script.js
@@ -23,11 +23,17 @@ function checkInputs() {
 
 
     if (usernameValue === '') {
-
         setErrorFor(username, 'Username cannot be blank')
-
     } else {
         setSuccessFor(username)
+    }
+    if (emailValue === '') {
+        setErrorFor(email, 'Email cannot be blank')
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'Email is not valid')
+    }
+    else {
+        setSuccessFor(email)
     }
 }
 
@@ -41,4 +47,8 @@ function setErrorFor(input, message) {
 function setSuccessFor(input) {
     const formControl = input.parentElement;
     formControl.className = 'form-control success'
+}
+
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
